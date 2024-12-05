01fe425e40ff8f57e8f86e8c5aef12dbb989d80c
raj kumar <79806602+raj921@users.noreply.github.com>
2024-12-05 22:33:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = 'AIzaSyAbEzlFgyk2xOSAjOJXtOThtG4JUhXOOqM';
+const GEMINI_API_KEY = '';
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
 
 // Initialize context menu items
